4059021034602a3f2daf323ccb17a9f67a83ec8d
itsmebasil8 <92202940+itsmebasil8@users.noreply.github.com>
2021-12-12 23:09:33 +0530

    ᴄᴀᴛʙᴏᴛ

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,10 +12,10 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*JulieMwol by Farhan_Dqz*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Catbot created by basilser*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n OWNER NUMBER : https://wa.me/*'Config.BSL+\n\n HE IS MY OWNER', MessageType.text);
         }
     }));
 }
@@ -30,10 +30,10 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*JulieMwol by Farhan_Dqz*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Catbot created by basilser*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n OWNER NUMBER : https://wa.me/*'Config.BSL+\n\n HE IS MY OWNER', MessageType.text);
         }
     }));
 }
