d21b5166b5761a70383aeb9995834469fb6cdcb6
itsmebasil8 <92202940+itsmebasil8@users.noreply.github.com>
2021-12-13 19:12:48 +0530

    ᴄᴀᴛʙᴏᴛ

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,1 +1,45 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const {spawnSync} = require('child_process');
+const Config = require('../config');
+const chalk = require('chalk');
 
+const Language = require('../language');
+const Lang = Language.getString('system_stats');
+
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+
+        if (message.jid === '15369524516-1612300121@g.us') {
+
+            return;
+        }
+
+        if (Config.OWNER == 'default') {
+            await message.client.sendMessage(message.jid,'CatBot created by *Basil sir*' , MessageType.text);
+        }
+        else {
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
+        }
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+
+        if (message.jid === '54218542512-1612300121@g.us') {
+
+            return;
+        }
+
+        if (Config.OWNER == 'default') {
+            await message.client.sendMessage(message.jid,'CatBot created by *Basil sir*' , MessageType.text);
+        }
+        else {
+            await message.client.sendMessage(message.jid,'ʜʏ ᴅᴜᴅᴇ....👋🏻\n\n```MY OWNER:``` *'+Config.OWNER + '*\n\n ```OWNER NUMBER``` *'+Config.BSL + '\n\n---------------------', MessageType.text);
+        }
+    }));
+}
